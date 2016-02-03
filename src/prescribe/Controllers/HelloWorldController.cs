8d2e312bc8bb55d1cd1b4a05f999fbb02af431b5
tonysolo <tony@soloworks.co.za>
2016-02-03 14:49:46 +0200

    0302

---
+++ src/prescribe/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace prescribe.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["message"] = "Hello " + "Tony";
+            ViewData["numtimes"] = 20;
+            return  View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //var x = ViewData.Add();
+            //ViewData.Model
+            ViewData["message"] = "Hello "+ name;
+            ViewData["numtimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
---
+++ src/prescribe/Models/datamodel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity.Design;
+
+namespace prescribe.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string FName { get; set; }
+        public string SName { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string str)
+        {
+            var stn = str.Split(',');
+            Id = stn[0];
+            FName = stn[1];
+            SName = stn[2];
+
+        }
+    }
+
+
+    public class Patient : Person
+    {
+        public Patient(string str)
+        {
+            var stn = str.Split(',');
+            Id = stn[0];
+            FName = stn[1];
+            SName = stn[2];
+        }
+
+    }
+
+    public class Provider : Person
+    {
+        public List<Patient> RecentPatients;
+    }
+
+}
+
